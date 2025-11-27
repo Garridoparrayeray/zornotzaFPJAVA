f894a1c3be48686a9c5fff96b6a3b99e080e011d
Garridoparrayeray <garridoparrayeray@gmail.com>
2025-11-27 16:52:10 +0100

    reorganizacion de repositorio

--- jarduera2_1/Jarduera2_1_1.java
+++
@@ -1,8 +0,0 @@
-package jarduera2_1;
-
-public class Jarduera2_1_1 {
-	public static void main(String[]args) {
-		//Erakutsi pantailatik "Kaixo mundua!" mezua.
-		System.out.println("Kaixo mundua");
-	}
-}
